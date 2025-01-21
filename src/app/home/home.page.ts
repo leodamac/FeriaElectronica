0e6dcb212a77f0add60a5e769d94cb5fc50a98d2
Leonardo Macias <105889369+leodamac@users.noreply.github.com>
2025-01-21 10:49:49 -0500

    Update home.page.ts

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, RouterLink],
 })
 export class HomePage {
-  constructor() {}
+  constructor() {
+  }
 }
