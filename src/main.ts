b80d4fb37e590ee3e1831674add95cdb881736fe
leodamac <105889369+leodamac@users.noreply.github.com>
2025-01-21 20:17:59 -0500

    aa

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+
 
 bootstrapApplication(AppComponent, {
   providers: [
@@ -11,4 +13,7 @@ bootstrapApplication(AppComponent, {
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
   ],
-});
+}).then(() => {
+  // Una vez que la aplicación se ha inicializado, carga los PWA Elements
+  defineCustomElements(window);
+}).catch(err => console.error(err));
